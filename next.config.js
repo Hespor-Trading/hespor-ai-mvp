07ad7b24bc21df2e6e76128e591fa980ea211b42
Hespor-Trading <info@hespor.com>
2025-09-17 19:25:26 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/undefined',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
+};
 
+module.exports = nextConfig;
