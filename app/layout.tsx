77cb41ff4ec86e285eec48a90aeee447c1ccc0de
Hespor-Trading <info@hespor.com>
2025-09-22 11:08:23 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "HESPOR",
-  description: "Hespor App",
+  description: "Hespor AI",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
