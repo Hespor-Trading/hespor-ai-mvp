04bb542f110a302562dc8f08ec623c391d35820b
Hespor-Trading <info@hespor.com>
2025-10-03 20:02:24 -0700

    Update page.tsx

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,5 +1,6 @@
 // app/auth/sign-in/page.tsx
 import NextDynamic from "next/dynamic";
+import CookieBridge from "./CookieBridge";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -7,5 +8,10 @@ export const revalidate = 0;
 const Client = NextDynamic(() => import("./Client"), { ssr: false });
 
 export default function Page() {
-  return <Client />;
+  return (
+    <>
+      <CookieBridge />
+      <Client />
+    </>
+  );
 }
