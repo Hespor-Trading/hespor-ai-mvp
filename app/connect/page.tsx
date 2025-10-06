eb7153457b1472ffa6d4372ffbc9dbe7c6af1c41
Hespor-Trading <info@hespor.com>
2025-10-06 10:38:21 -0700

    Update page.tsx

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,25 +1,10 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { createServerClient } from "@supabase/ssr";
 import NextDynamic from "next/dynamic";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-const ConnectClient = NextDynamic(() => import("./Client"), { ssr: false });
+const Client = NextDynamic(() => import("./Client"), { ssr: false });
 
-export default async function Page() {
-  const cookieStore = cookies();
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: { get: (name) => cookieStore.get(name)?.value } }
-  );
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (!session) redirect(`/auth/sign-in?next=/connect`);
-  return <ConnectClient />;
+export default function Page() {
+  return <Client />;
 }
